6ce3c61831f859e560588beee5bc5ad5e18509fb
Francois Wieckowiak <legentil42@gmail.com>
2021-11-15 15:54:14 +0000

    week 7 uob session

--- create_distal_T_ij.m
+++ create_distal_T_ij.m
@@ -1,8 +1,8 @@
 function T_ij = create_distal_T_ij(a,alpha,d,theta)
-ca = cos(alpha);
-sa = sin(alpha);
-ct = cos(theta);
-st = sin(theta);
+ca = cosd(alpha);
+sa = sind(alpha);
+ct = cosd(theta);
+st = sind(theta);
 
 T_ij = [ct -ca*st sa*st a*ct ; st ca*ct -sa*ct a*st ; 0 sa ca d; 0 0 0 1];
 end
